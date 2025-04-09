9f9fab39732553f501a12a1126c9bb0d5aa823ec
cry4alone <chernenkoa04@gmail.com>
2025-04-09 10:29:02 +0700

    added tests

--- csharp course/Program.cs
+++ csharp course/Program.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 
 namespace csharp_course
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             float a = 10;
+            Console.WriteLine(Perimeter(a));
+        }
+
+        public static float Perimeter(float a)
+        {
             float P = 4 * a;
-            Console.WriteLine(P);
+            return P;
         }
     }
 }
---
+++ csharp courseTests1/ProgramTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csharp_course;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_course.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        [DataTestMethod]
+        [DataRow(10, 40)]
+        [DataRow(5, 20)]
+        public void PerimeterTest(float input, float expected)
+        {
+            float result = Program.Perimeter(input);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}

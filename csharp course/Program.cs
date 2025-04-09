d5be98dc91bb6f7b4f4a8cce2ba99c9c4ae9506d
cry4alone <chernenkoa04@gmail.com>
2025-04-09 09:01:13 +0700

    series

--- csharp course/Program.cs
+++ csharp course/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +11,13 @@ namespace csharp_course
     {
         static void Main(string[] args)
         {
-            int A = 0; int B = 5; int C = 2;
-            if (A < B && B < C)
+            float[] nums = new float[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            float sum = 0;
+            foreach (float x in nums)
             {
-                Console.WriteLine("True");
+                sum += x;
             }
-            else
-            {
-                Console.WriteLine("False");
-            }
+            Console.WriteLine(sum);
         }
     }
 }
